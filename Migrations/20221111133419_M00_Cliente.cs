aa21d1c8ded41b1559dc297a83945273b69d1cbb
Fernando Alves <fernends@yahoo.com>
2022-11-11 10:57:34 -0300

    Update

---
+++ Migrations/20221111133419_M00_Cliente.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoEasypark.Migrations
+{
+    public partial class M00_Cliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Cliente_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoCarro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModeloCarro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlacaCarro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Registro = table.Column<int>(type: "int", nullable: false),
+                    TipoVaga = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Cliente_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
